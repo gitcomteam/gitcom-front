7cc84749a8bc0ac489e6bd339ced4cd7d1b33b2f
Max Kovalenko <mxsshere@gmail.com>
2020-01-05 19:58:20 -0500

    added home section

--- src/layouts/home/main/HomeMainLayout.tsx
+++ src/layouts/home/main/HomeMainLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import FullPageWithSideBar from "../../../components/layout/simple/fullpagewithsidebar/FullPageWithSidebar";
+import {Link} from "react-router-dom";
 
 interface IProps {
 }
@@ -16,7 +17,24 @@ class HomeMainLayout extends React.Component<IProps, IState> {
     render() {
         return <FullPageWithSideBar sidebarType={"home"}>
             <div>
-                Home layout
+                <h2 className={"ant-typography"}>Home layout</h2>
+                <b>Welcome to the GitCom platform!</b>
+                <div className="text-left">
+                    <b>If you're a user:</b>
+                    <p>
+                        Add projects that you like to your library, then your monthly subscription will be distributed
+                        across your favorite projects.
+                    </p>
+
+                    <br/>
+
+                    <b>If you're a developer:</b>
+                    <p>
+                        Go to <Link to={"/home/integrations"}>integrations</Link> and connect your GitHub or GitLab account
+                        then you can import your projects and set up project boards and pricing plans
+                        (can be found on project page)!
+                    </p>
+                </div>
             </div>
         </FullPageWithSideBar>;
     }
